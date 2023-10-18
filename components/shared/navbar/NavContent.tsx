3edfbcd1ae9167da322d5be6ecb8d47b5272e164
victorbobkov <victorbobkov@gmail.com>
2023-10-18 03:41:17 +0300

    Create Navbar

---
+++ components/shared/navbar/NavContent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavContent = () => {
+  return <div>NavContent</div>;
+};
+
+export default NavContent;
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { createContext, useContext, useState, useEffect } from "react";
 
 interface ThemeContextType {
@@ -12,18 +13,24 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-      document.documentElement.classList.add("light");
-    } else {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   };
 
   useEffect(() => {
     handleThemeChange();
   }, [mode]);
+
+  console.log("MODE, ", mode);
 
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
@@ -38,4 +45,6 @@ export function useTheme() {
   if (context === undefined) {
     throw new Error("useTheme must be used within a ThemeProvider");
   }
+
+  return context;
 }
